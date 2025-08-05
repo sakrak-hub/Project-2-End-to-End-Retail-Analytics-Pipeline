826fcb0d6faa9beeb724548d1838b203657a9f19
sakrak-hub <sakra_k@outlook.com>
2025-08-05 10:18:25 +0800

    Daily Commit

--- scripts/upload_to_s3_cron.sh
+++ scripts/upload_to_s3_cron.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker-compose up
+PROJECT_URL="/mnt/d/Projects/Project-2-End-to-End-Retail-Analytics-Pipeline/"
+
+mv ${PROJECT_URL}.env ${PROJECT_URL}secrets/.env
+echo "env file moved to secrets"
+git add .
+git commit -m "Daily Commit"
+git push origin main
+
+mv ${PROJECT_URL}secrets/.env ${PROJECT_URL}.env
+echo "env file moved to main"
+
+cd ${PROJECT_URL}; docker-compose up
