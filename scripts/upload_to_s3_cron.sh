9d1d31d20ca7b9b112fb92a600c34a70a531e1ad
sakrak-hub <sakra_k@outlook.com>
2025-08-25 10:02:19 +0800

    Daily Commit

--- scripts/upload_to_s3_cron.sh
+++ scripts/upload_to_s3_cron.sh
@@ -2,6 +2,7 @@
 
 PROJECT_URL="/mnt/d/Projects/Project-2-End-to-End-Retail-Analytics-Pipeline/"
 
+source ${PROJECT_URL}/.venv/bin/activate
 mv ${PROJECT_URL}.env ${PROJECT_URL}secrets/.env
 echo "env file moved to secrets"
 git add .
